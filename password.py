a7b584451bf725e4e3b3bfbd9727a7d4aeb305ab
Andrew Johnson <johnand4@flip1.engr.oregonstate.edu>
2021-04-16 17:51:44 -0700

    fix error with creating passwords longer than 1

--- password.py
+++ password.py
@@ -2,5 +2,6 @@ import string
 from random import *
 
 def password (length):
+	password = ""
 	for x in range(length):
-		password =  "".join(choice(string.ascii_letters + string.punctuation  + string.digits))
+		password +=  "".join(choice(string.ascii_letters + string.punctuation  + string.digits))
